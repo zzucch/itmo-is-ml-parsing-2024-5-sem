6815830b4002518e35fb238b28a93db0c100dc49
zzucch <nikitamilovatskiy@gmail.com>
2024-09-22 19:16:40 +0300

    feat: add id parsing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    let url = "https://jimaku.cc";
+    let url = "https://jimaku.cc/dramas";
 
     let Ok(body) = request::get_body(url).await else {
         todo!()
--- src/parse.rs
+++ src/parse.rs
@@ -1,11 +1,14 @@
 use scraper::{Html, Selector};
+use serde::Deserialize;
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Entry {
+    #[serde(skip_deserializing)]
+    id: i32,
     name: String,
-    flags: u64,
-    last_modified: u64,
-    anilist_id: Option<u64>,
+    flags: i32,
+    last_modified: i64,
+    anilist_id: Option<i32>,
     tmdb_id: Option<String>,
     english_name: Option<String>,
     japanese_name: Option<String>,
@@ -16,14 +19,41 @@ pub fn parse(data: &str) {
     let selector = Selector::parse("div.entry").unwrap();
 
     for element in document.select(&selector) {
-        if let Some(data_extra) = element.value().attr("data-extra") {
-            let entry = parse_data_extra(data_extra);
-            println!("{:?}", entry);
+        let data_extra = match element.value().attr("data-extra") {
+            Some(data) => data,
+            None => continue,
+        };
+
+        let mut entry = parse_data_extra(data_extra);
+
+        if let Some(id) = parse_id_from_element(&element) {
+            entry.id = id;
+        } else {
+            continue;
         }
+
+        println!("{:?}", entry);
     }
 }
 
 fn parse_data_extra(data_extra: &str) -> Entry {
     let data_extra_decoded = data_extra.replace("&quot;", "\"");
     serde_json::from_str(&data_extra_decoded).unwrap()
+}
+
+fn parse_id_from_element(element: &scraper::ElementRef) -> Option<i32> {
+    let link = match element
+        .select(&Selector::parse("a.table-data.file-name").unwrap())
+        .next()
+    {
+        Some(link) => link,
+        None => return None,
+    };
+
+    let href = match link.value().attr("href") {
+        Some(href) => href,
+        None => return None,
+    };
+
+    href.split('/').last().and_then(|s| s.parse::<i32>().ok())
 }
